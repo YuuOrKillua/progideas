9ee689ef49eb0e28b62c3811add54f6252361170
Monim <Dzardo008@gmail.com>
2023-11-29 17:28:22 -0400

    implementacão monim

---
+++ src/Monim.js
@@ -0,0 +1,9 @@
+function Monim() {
+    return(
+        <div className="Monim">
+            <p>oal</p>
+        </div>
+    );
+}
+
+export default Monim;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Monim from  './Monim';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Monim />
   </React.StrictMode>
 );
