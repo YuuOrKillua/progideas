c29050df8852e85d0e43bc2c6e2e5b8d2d766b00
YuuOrKillua <barbosa.c3.bb@gmail.com>
2023-11-30 19:31:36 -0300

    Changing placeholder color for all browsers

--- src/Components/Valorant.js
+++ src/Components/Valorant.js
@@ -3,7 +3,7 @@ import '../ComponentStyles/ValorantStyle.css';
 function Valorant(){
     return(
         <div className='Valorant'>
-            <h1 id='Main-Tiltle'>Valorante Show Room</h1>
+            <h1 id='Main-Tiltle'>Valorant Show Room</h1>
             <input id='SearchBox' type='text' placeholder='Search your desire skin here'/>
             <h2 id='Comum-Title'>Newest</h2>
             <h2 id='Comum-Title'>Hottest</h2>
